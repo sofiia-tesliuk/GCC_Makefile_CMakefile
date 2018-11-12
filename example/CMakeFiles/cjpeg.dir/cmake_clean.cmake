783b21d36763105cf80c1db38f0d45ef5e225ee8
sophia-tslk <teslyuk.sofia@gmail.com>
2018-11-13 00:39:40 +0200

    add cmakelists;

---
+++ example/CMakeFiles/cjpeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjpeg.dir/cjpeg.c.o"
+  "cjpeg.pdb"
+  "cjpeg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cjpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ example/CMakeFiles/djpeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/djpeg.dir/djpeg.c.o"
+  "djpeg.pdb"
+  "djpeg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/djpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
